4bcdd4b7938a1421cff9d5b98eb78f29fc62a97e
Jones <jonesandjay123@gmail.com>
2018-08-10 09:29:44 +0800

    second commit

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
 firebase.initializeApp({
-    apiKey: "AIzaSyCl_SZpy_jPglg0T0f4a6uxafyNDYLU-q4",
-	authDomain: "test-2292b.firebaseapp.com",
-    databaseURL: "https://test-2292b.firebaseio.com",
-    projectId: "test-2292b",
-    storageBucket: "test-2292b.appspot.com",
-    messagingSenderId: "289972332728"
+  apiKey: "AIzaSyCKnwCMHxdtgftBbrCxheQ8YaLKCGBnNMI",
+  authDomain: "luckydraw-a74fa.firebaseapp.com",
+  databaseURL: "https://luckydraw-a74fa.firebaseio.com",
+  projectId: "luckydraw-a74fa",
+  storageBucket: "luckydraw-a74fa.appspot.com",
+  messagingSenderId: "419044534265"
 });
